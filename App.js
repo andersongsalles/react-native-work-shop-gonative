001e08555de2738b7878fed52621939ced90b608
Anderson Salles <agsalles@gmail.com>
2018-01-09 17:27:07 -0200

    commit inicial projeto lista repositories do github

---
+++ App.js
@@ -0,0 +1,117 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+  TouchableOpacity,
+  AsyncStorage,
+} from 'react-native';
+
+import Repo from './components/Repo';
+import NewRepoModal from './components/NewRepoModal';
+
+
+export default class App extends Component<{}> {
+
+  state = {
+    modalVisible: false,
+    repos: [],
+  };
+
+  async componentDidMount(){
+    const repos =  JSON.parse(await AsyncStorage.getItem('@Minicurso:repositories')) || [];
+
+    this.setState({ repos })
+  }
+
+  _addRepository = async (newRepoText) => {
+    const repoCall = await fetch(`http://api.github.com/repos/${newRepoText}`)
+    const response = await repoCall.json();
+
+    const repository = {
+      id: response.id,
+      thumbnail: response.owner.avatar_url,
+      title: response.name,
+      author: response.owner.login
+    };
+
+    this.setState({
+      modalVisible: false,
+      repos: [
+        ...this.state.repos,
+        repository,
+      ],
+    });
+
+    await AsyncStorage.setItem('@Minicurso:repositories', JSON.stringify(this.state.repos))
+  };
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.headerText}>Minicurso Gonative</Text>
+            <TouchableOpacity onPress={() => this.setState({ modalVisible: true })}>
+              <Text style={styles.headerButton}>+</Text>
+            </TouchableOpacity>
+          </View>
+
+        <ScrollView contentContainerStyle={styles.repoList}>
+          { this.state.repos.map(repo => <Repo key={repo.id} data={repo} />)}
+        </ScrollView>
+
+        <NewRepoModal
+          onCancel={() => this.setState({ modalVisible: false })}
+          onAdd={this._addRepository}
+          visible={this.state.modalVisible}
+        />
+
+
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+  },
+
+  header: {
+    height: (Platform.OS === 'ios') ? 70 : 50,
+    paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+
+  headerButton: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  headerText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  repoList: {
+    padding: 20,
+  },
+
+
+});
